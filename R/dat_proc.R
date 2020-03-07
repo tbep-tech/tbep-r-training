1ef85819b0a901e9afc6f8c19c3e6d1bbaa70823
fawda123 <mbafs2012@gmail.com>
2020-03-07 09:09:41 -0500

    otb fim data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -3,7 +3,8 @@ library(tbeptools)
 
 fimdat <- st_read('https://opendata.arcgis.com/datasets/68fb6774b58a40a08a6f87959aaa53c4_26.geojson')
 
-tbfimdat <- fimdat[tbseg, ]
-tbfimdat <- st_set_geometry(tbfimdat, NULL)
+otb <- tbseg[tbseg$bay_segment %in% 'OTB',]
+otbfimdat <- fimdat[otb, ]
+otbfimdat <- st_set_geometry(otbfimdat, NULL)
 
-write.csv(tbfimdat, 'data/tbfimdat.csv', row.names = F)
+write.csv(otbfimdat, 'data/otbfimdat.csv', row.names = F)

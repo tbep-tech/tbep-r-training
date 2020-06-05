c366bec21e662c1183f413d6c5503f86ead89f5b
fawda123 <mbafs2012@gmail.com>
2020-06-05 08:41:32 -0400

    data updates

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,25 +1,42 @@
 library(sf)
 library(tbeptools)
 
-fimdat <- st_read('https://opendata.arcgis.com/datasets/68fb6774b58a40a08a6f87959aaa53c4_26.geojson')
+# FIM csv data ------------------------------------------------------------
+
+# fimdat <- st_read('https://opendata.arcgis.com/datasets/68fb6774b58a40a08a6f87959aaa53c4_26.geojson')
 
 otb <- tbseg[tbseg$bay_segment %in% 'OTB',]
-otbclip <- fimdat[otb, ]
-otbfimdatcmb <- st_set_geometry(otbclip, NULL) %>% 
-  select(-Scientificname, -avg_size, -min_size, -max_size) %>% 
-  filter(Commonname %in% c('Sand Seatrout', 'Red Drum', 'Pinfish', 'Common Snook', 'Bluefish', 'Mullets')) %>% 
-  spread(Commonname, TotalNum, fill = 0)
+# otbclip <- fimdat[otb, ]
+# otbfimdatcmb <- st_set_geometry(otbclip, NULL) %>% 
+#   select(-Scientificname, -avg_size, -min_size, -max_size) %>% 
+#   filter(Commonname %in% c('Sand Seatrout', 'Red Drum', 'Pinfish', 'Common Snook', 'Bluefish', 'Mullets')) %>% 
+#   spread(Commonname, TotalNum, fill = 0)
+# 
+# write.csv(otbfimdatcmb, 'data/otbfimdatcmb.csv', row.names = F)
 
-statloc <- otbfimdatcmb %>% 
-  select(Reference, Latitude, Longitude) %>% 
-  unique()
-
-write.csv(otbfimdatcmb, 'data/otbfimdatcmb.csv', row.names = F)
+otbfimdatcmb <- read.csv('data/otbfimdatcmb.csv')
 
 fishdat <- otbfimdatcmb %>% 
   select(-Latitude, -Longitude)
 
+statloc <- otbfimdatcmb %>%
+  select(Reference, Latitude, Longitude) %>%
+  unique()
+
 write.csv(fishdat, 'data/fishdat.csv', row.names = F)
 write.csv(statloc, 'data/statloc.csv', row.names = F)
 
-zip('data/datazip.zip', c('data/otbfimdat.csv', 'data/statloc.csv'))
+# seagrass spatial data ---------------------------------------------------
+
+load(file = '../seagrass-dash/data/sgdat2016.RData')
+
+sgdat <- sgdat2016[otb, ] %>% 
+  select(OBJECTID, FLUCCS_CODE)
+
+st_write(sgdat, 'data/sgdat.shp', delete_layer = T)
+
+# zip all data ------------------------------------------------------------
+
+fls <- list.files('.', pattern = '^sgdat|^fishdat\\.csv$|^statloc\\.csv$', recursive = T)
+
+zip('data/data.zip', fls)
--- buildfile.R
+++ buildfile.R
@@ -2,10 +2,12 @@
 torm <- list.files(pattern = '\\.R$', recursive = F)
 torm <- torm[!torm %in% c('index.R', 'buildfile.R')]
 file.remove(torm)
+file.remove('data/data.zip')
 rmarkdown::clean_site()
 
 # build
 rmarkdown::render_site()
+source('R/dat_proc.R')
 tobld <- list.files(pattern = '\\.Rmd$', recursive = F)
 tobld <- tobld[!tobld %in% c('Resources.Rmd', 'index.Rmd')]
 sapply(tobld, knitr::purl, documentation = 0L)

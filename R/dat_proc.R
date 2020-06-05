92e2f86aea5f7023b894e28cbe6259f7f3fd655f
fawda123 <mbafs2012@gmail.com>
2020-06-05 11:42:33 -0400

    mapping, resources added

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,23 +1,26 @@
 library(sf)
 library(tbeptools)
+library(lubridate)
 
 # FIM csv data ------------------------------------------------------------
 
 # fimdat <- st_read('https://opendata.arcgis.com/datasets/68fb6774b58a40a08a6f87959aaa53c4_26.geojson')
-
-otb <- tbseg[tbseg$bay_segment %in% 'OTB',]
+# 
+# otb <- tbseg[tbseg$bay_segment %in% 'OTB',]
 # otbclip <- fimdat[otb, ]
-# otbfimdatcmb <- st_set_geometry(otbclip, NULL) %>% 
-#   select(-Scientificname, -avg_size, -min_size, -max_size) %>% 
-#   filter(Commonname %in% c('Sand Seatrout', 'Red Drum', 'Pinfish', 'Common Snook', 'Bluefish', 'Mullets')) %>% 
+# otbfimdatcmb <- st_set_geometry(otbclip, NULL) %>%
+#   select(-Scientificname, -avg_size, -min_size, -max_size) %>%
+#   filter(Commonname %in% c('Sand Seatrout', 'Red Drum', 'Pinfish', 'Common Snook', 'Bluefish', 'Mullets')) %>%
 #   spread(Commonname, TotalNum, fill = 0)
 # 
-# write.csv(otbfimdatcmb, 'data/otbfimdatcmb.csv', row.names = F)
+# write_csv(otbfimdatcmb, 'data/otbfimdatcmb.csv')
 
-otbfimdatcmb <- read.csv('data/otbfimdatcmb.csv')
+otbfimdatcmb <- read_csv('data/otbfimdatcmb.csv')
 
 fishdat <- otbfimdatcmb %>% 
-  select(-Latitude, -Longitude)
+  select(-Latitude, -Longitude) %>% 
+  mutate(yr = year(Sampling_Date)) %>% 
+  select(OBJECTID, Reference, Sampling_Date, yr, Gear, ExDate, Bluefish, `Common Snook`, Mullets, Pinfish, `Red Drum`, `Sand Seatrout`)
 
 statloc <- otbfimdatcmb %>%
   select(Reference, Latitude, Longitude) %>%
@@ -31,7 +34,7 @@ write.csv(statloc, 'data/statloc.csv', row.names = F)
 load(file = '../seagrass-dash/data/sgdat2016.RData')
 
 sgdat <- sgdat2016[otb, ] %>% 
-  select(OBJECTID, FLUCCS_CODE)
+  select(OBJECTID, FLUCCS = FLUCCS_CODE)
 
 st_write(sgdat, 'data/sgdat.shp', delete_layer = T)
 
--- mapping.R
+++ mapping.R
@@ -1,1 +1,95 @@
+# load libraries
+library(tidyverse)
+library(sf)
+library(mapview)
 
+# load the fish data
+fishdat <- read_csv('data/fishdat.csv')
+
+# load the station data
+statloc <- read_csv('data/statloc.csv')
+
+# load the sgdat shapefile
+sgdat <- st_read('data/sgdat.shp')
+
+methods(class = 'sf')
+
+sgdat <- st_read('data/sgdat.shp', quiet = T)
+sgdat
+
+str(fishdat)
+str(statloc)
+
+# Join the data
+alldat <- left_join(fishdat, statloc, by = 'Reference')
+
+# check row counts
+nrow(fishdat)
+nrow(statloc)
+nrow(alldat)
+
+# create spatial data object
+alldat <- st_as_sf(alldat, coords = c('Longitude', 'Latitude'), crs = 4326)
+
+# examine the sf objec
+alldat
+str(alldat)
+
+# check crs
+st_crs(alldat)
+
+# verify the polygon and point data have the same crs
+st_crs(sgdat)
+st_crs(alldat)
+
+alldatutm <- alldat %>% 
+  st_transform(crs = '+proj=utm +zone=17 +datum=NAD83 +units=m +no_defs')
+st_crs(alldatutm)
+
+plot(alldat)
+plot(sgdat)
+
+plot(alldat$geometry)
+plot(sgdat$geometry)
+
+filt_dat <- alldat %>% 
+  filter(yr == 2016)
+plot(filt_dat$geometry)
+
+fish_sub <- filt_dat[sgdat, ]
+plot(fish_sub$geometry)
+fish_sub
+
+fish_sub <- st_intersection(alldat, sgdat)
+plot(fish_sub$geometry)
+fish_sub
+
+fish_cnt <- fish_sub %>% 
+  group_by(FLUCCS) %>% 
+  summarise(
+    cnt = sum(Pinfish)
+  ) 
+fish_cnt
+
+ggplot(fish_cnt, aes(x = FLUCCS, y = cnt)) + 
+  geom_bar(stat = 'identity')
+
+# use ggplot with sf objects
+ggplot() + 
+  geom_sf(data = sgdat, fill = 'green') + 
+  geom_sf(data = alldat, aes(fill = 'Gear')) 
+
+mapview(sgdat, col.regions = 'green') +
+  mapview(alldat, zcol = 'Gear')
+
+# filter alldat 
+tomap <- alldat %>% 
+  filter(FLUCCS == 9116) %>% 
+  filter(yr == 2016)
+
+# make map
+mapview(tomap, zcol = 'Pinfish')
+mapview(tomap, zcol = 'Bluefish')
+
+# join maps
+mapview(sgdat) + mapvew(tomap, zcol = 'Pinfish')

86dd6aa91ef0015bc177ec9105b8d6944b00b626
fawda123 <mbafs2012@gmail.com>
2020-06-07 11:45:23 -0400

    rebuild site with live coding links, removed live coding file dropbox only

--- live_coding.R
+++
@@ -1,7 +0,0 @@
-
-# live coding script ------------------------------------------------------
-
-# Website: https://tbep-tech.github.io/tbep-r-training/index.html
-# Live Coding Link: https://tinyurl.com/live_coding
-# Data: https://tbep-tech.github.io/tbep-r-training/data/data.zip
-

eaacbebe679bb7f060b90c796938c450523cb1be
fawda123 <mbafs2012@gmail.com>
2020-06-07 11:32:40 -0400

    live coding script added

---
+++ live_coding.R
@@ -0,0 +1,7 @@
+
+# live coding script ------------------------------------------------------
+
+# Website: https://tbep-tech.github.io/tbep-r-training/index.html
+# Live Coding Link: https://tinyurl.com/live_coding
+# Data: https://tbep-tech.github.io/tbep-r-training/data/data.zip
+

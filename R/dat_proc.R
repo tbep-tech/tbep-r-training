9328282de17aba06e1f34e42e1d5027d4e99edfa
fawda123 <mbafs2012@gmail.com>
2020-08-07 07:54:50 -0400

    registrant bar plot to dat_proc

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,6 +2,10 @@ library(sf)
 library(tbeptools)
 library(lubridate)
 library(tidyverse)
+library(extrafont)
+
+loadfonts(device = 'win', quiet = T)
+fml <- 'Lato'
 
 # FIM csv data ------------------------------------------------------------
 
@@ -38,3 +42,44 @@ sgdat <- sgdat2016[otb, ] %>%
   select(OBJECTID, FLUCCS = FLUCCS_CODE)
 
 st_write(sgdat, 'data/sgdat.shp', delete_layer = T)
+
+# registrant counts -------------------------------------------------------
+
+toplo <- read.csv('~/Desktop/TBEP R training RSVP.csv') %>% 
+  janitor::clean_names() %>% 
+  select(email = matches('^please_provide_your_email')) %>%
+  mutate(
+    email = tolower(email), 
+    email = gsub('^.*\\@', '', email), 
+    email = gsub('\\,', '.', email), 
+    email = gsub('\\.com$|\\.edu$|\\.gov$|\\.org$|\\.net$|\\.con$', '', email), 
+    email = gsub('\\.com\\.au$', '', email), 
+    email = gsub('contractor\\.', '', email), 
+    email = gsub('mail\\.', '', email)
+  ) %>% 
+  group_by(email) %>% 
+  summarise(n = n()) %>%
+  arrange(-n) %>% 
+  mutate(email = factor(email, levels = email))
+
+p <- ggplot(toplo, aes(x = email, y = n)) + 
+  geom_bar(stat = 'identity') + 
+  theme_minimal() +
+  scale_y_continuous(expand = c(0, 0)) + 
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(), 
+    panel.grid.minor.y = element_blank(), 
+    axis.title.x = element_blank(), 
+    axis.text.x = element_text(size = 6, angle = 40, hjust = 1), 
+    text = element_text(family = fml)
+  ) + 
+  labs(
+    y = 'Count', 
+    title = 'Workshop registrants by email domain'
+  )
+
+png('~/Desktop/registrants.png', height = 3.5, width = 8, family = fml, units = 'in', res = 200)
+print(p)
+dev.off()
+
